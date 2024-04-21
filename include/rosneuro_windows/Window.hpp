47af007eeb97ae64f5a10a5cd78a73d263b61d8d
mirovix <miroljubmihailovic98@gmail.com>
2024-04-21 13:33:44 +0200

    refactor: change function name.

--- include/rosneuro_windows/Window.hpp
+++ include/rosneuro_windows/Window.hpp
@@ -7,33 +7,32 @@
 #include <gtest/gtest_prod.h>
 
 namespace rosneuro {
-
-template <typename T>
-class Window : public Filter<T> {
-	public:
-		Window(void);
-		~Window(void) {};
-		virtual bool configure(void) = 0;
-		virtual DynamicMatrix<T> apply(const DynamicMatrix<T>& in) = 0;
-		T GetWindowNorm();
+    template <typename T>
+    class Window : public Filter<T> {
+        public:
+            Window(void);
+            ~Window(void) {};
+            virtual bool configure(void) = 0;
+            virtual DynamicMatrix<T> apply(const DynamicMatrix<T>& in) = 0;
+            T GetWindowNorm();
 
 	protected:
 		bool is_window_set_;
 		T wnorm_;
 
         FRIEND_TEST(WindowTestSuite, Constructor);
-};
+    };
 
-template<typename T>
-Window<T>::Window(void) {
-	this->name_ 	     = "Window";
-	this->is_window_set_ = false;
-}
+    template<typename T>
+    Window<T>::Window(void) {
+        this->name_ 	     = "Window";
+        this->is_window_set_ = false;
+    }
 
-template<typename T>
-T Window<T>::GetWindowNorm(){
-	return this->wnorm_;
-}
+    template<typename T>
+    T Window<T>::GetWindowNorm(){
+        return this->wnorm_;
+    }
 
 }
 
--- test/TestWindow.cpp
+++ test/TestWindow.cpp
@@ -2,44 +2,42 @@
 #include "rosneuro_windows/Window.hpp"
 
 namespace rosneuro {
+    template <typename T>
+    class WindowTest : public Window<T> {
+        public:
+            WindowTest  () : Window<T>() {}
+            ~WindowTest() {}
+            bool configure() { return true; }
+            DynamicMatrix<T> apply(const DynamicMatrix<T>& in) { return in; }
+    };
 
-template <typename T>
-class WindowTest : public Window<T> {
-    public:
-        WindowTest() : Window<T>() {}
-        ~WindowTest() {}
-        bool configure() { return true; }
-        DynamicMatrix<T> apply(const DynamicMatrix<T>& in) { return in; }
-};
+    class WindowTestSuite : public ::testing::Test {
+        public:
+            WindowTestSuite() {}
+            ~WindowTestSuite() {}
+            void SetUp() { window_filter = new WindowTest <double>(); }
+            void TearDown() { delete window_filter; }
+            WindowTest <double>* window_filter;
+    };
 
-class WindowTestSuite : public ::testing::Test {
-    public:
-        WindowTestSuite() {}
-        ~WindowTestSuite() {}
-        void SetUp() { window_filter = new WindowTest <double>(); }
-        void TearDown() { delete window_filter; }
-        WindowTest <double>* window_filter;
-};
+    TEST_F(WindowTestSuite, Constructor) {
+        ASSERT_EQ(window_filter->name_, "Window");
+        ASSERT_FALSE(window_filter->is_window_set_);
+    }
 
-TEST_F(WindowTestSuite, Constructor) {
-    ASSERT_EQ(window_filter->name_, "Window");
-    ASSERT_FALSE(window_filter->is_window_set_);
-}
+    TEST_F(WindowTestSuite, GetWindowNorm) {
+        ASSERT_EQ(window_filter->GetWindowNorm(), 0);
+    }
 
-TEST_F(WindowTestSuite, GetWindowNorm) {
-    ASSERT_EQ(window_filter->GetWindowNorm(), 0);
-}
+    TEST_F(WindowTestSuite, Configure) {
+        ASSERT_TRUE(window_filter->configure());
+    }
 
-TEST_F(WindowTestSuite, Configure) {
-    ASSERT_TRUE(window_filter->configure());
-}
-
-TEST_F(WindowTestSuite, Apply) {
-    DynamicMatrix<double> input(2, 2);
-    DynamicMatrix<double> output = window_filter->apply(input);
-    ASSERT_EQ(output, input);
-}
-
+    TEST_F(WindowTestSuite, Apply) {
+        DynamicMatrix<double> input(2, 2);
+        DynamicMatrix<double> output = window_filter->apply(input);
+        ASSERT_EQ(output, input);
+    }
 }
 
 int main(int argc, char **argv) {
